b091566597081759baf3963f7b04ec7a01b92c15
浅野未央 <mio@asanomionoMacBook-Pro.local>
2017-07-12 23:56:21 +0900

    firebase途中

--- Mymap/ViewController.swift
+++ Mymap/ViewController.swift
@@ -9,186 +9,194 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Firebase
 
-class ViewController: UIViewController ,UITextFieldDelegate , MKMapViewDelegate , CLLocationManagerDelegate{
-  //プロトコル
-  var locationManager: CLLocationManager!
+class ViewController: UIViewController ,UITextFieldDelegate , MKMapViewDelegate , CLLocationManagerDelegate {
   
+  // Todoモデルのインスタンス生成
+//  var TodoLists = [Todo]()
+  
+  // Firebaseのインスタンス生成
+  var ref: DatabaseReference!
+  private var databaseHandle: DatabaseHandle!
+  
+  var locationManager: CLLocationManager!
+  var location: CLLocationCoordinate2D!
+  var pin = MKPointAnnotation()
+  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-   
-  // Do any additional setup after loading the view, typically from a nib.
     
+    // 現在地にフォーカスを合わせる
     dispMap.setCenter(dispMap.userLocation.coordinate, animated: true)
+    // ユーザの位置に追従させる
     dispMap.userTrackingMode = MKUserTrackingMode.follow
     
     // Text Fieldのdelegate通知先を設定
     inputText.delegate = self
     
     // タップした時のアクションを追加
-    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapTapped(_:)))
+    let tapGesture = UILongPressGestureRecognizer(target: self, action: #selector(mapTapped(_:)))
     dispMap.addGestureRecognizer(tapGesture)
     
     // MapView Delegate設定
     dispMap.delegate = self
   }
   
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   
-
-
+  // inputText
   @IBOutlet weak var inputText: UITextField!
-
+  
+  // dispMap
   @IBOutlet weak var dispMap: MKMapView!
-
-  var memo: CLLocationCoordinate2D!
   
-  func setupLocationManager() {
-    locationManager = CLLocationManager()
-    guard let locationManager = locationManager else { return }
-    
-    locationManager.requestWhenInUseAuthorization()
-    
-    let status = CLLocationManager.authorizationStatus()
-    if status == .authorizedWhenInUse {
-      locationManager.distanceFilter = 10
-      locationManager.startUpdatingLocation()
-    }
-  }
+  //var memo: CLLocationCoordinate2D!
+  
+//  func setupLocationManager() {
+//    locationManager = CLLocationManager()
+//    guard let locationManager = locationManager else { return }
+//    
+//    locationManager.requestWhenInUseAuthorization()
+//    
+//    let status = CLLocationManager.authorizationStatus()
+//    if status == .authorizedWhenInUse {
+//      locationManager.distanceFilter = 10
+//      locationManager.startUpdatingLocation()
+//    }
+//  }
   
   
-  func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-    
-    let dialog = UIAlertController(title: "TO DO App", message: "やること", preferredStyle: .alert)
-    
-    let okAction = UIAlertAction(title: "OK", style: .default){(action)in
-      
-      let userInput = dialog.textFields?.first?.text!
-      
-      let pin = MKPointAnnotation()
-      pin.coordinate = self.memo
-    
-      pin.title = userInput
-      self.dispMap.addAnnotation(pin)
-      self.dispMap.region = MKCoordinateRegionMakeWithDistance( self.memo,500.0, 500.0 )
-
-      print(userInput ?? "no date")
-      
-      if (userInput?.isEmpty)!{
-        return
-      }
-    }
-    dialog.addTextField(configurationHandler: nil)
-    dialog.addAction(okAction)
-    
-    present(dialog,animated: true, completion: nil)
-    
-  }
-  
-
   
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    //Bool型
-    textField.resignFirstResponder()
-  
-    let searchKeyword = textField.text
-    //定数
     
+    // キーボードを閉じる(1)  Bool型
+    textField.resignFirstResponder()
+    
+    // 入力された文字を取り出す(2) 定数
+    let searchKeyword = textField.text
+    
+    // 入力された文字をデバックエリアに表示(3)
     print(searchKeyword ?? "値が入っていません")
     
+    // CLGeocoderインスタンスを取得(5)
     let geocodeder = CLGeocoder()
     
-    //入力された文字から情報を取得
+    //入力された文字から情報を取得(6)
     geocodeder.geocodeAddressString(searchKeyword!, completionHandler: { (Placemark:[CLPlacemark]?, error:Error?) in
       
-      //位置情報を取得する場合1件目の情報を取り出す
+      //位置情報を取得する場合1件目の情報を取り出す(7)
       if let placemark = Placemark?[0] {
         
-        //位置情報から緯度経度をtargetCoordinateに取り出す
+        //位置情報から緯度経度をtargetCoordinateに取り出す(8)
         if let tergetCoordnate = placemark.location?.coordinate{
           
-          // 緯度経度をデバックエリアに表示
+          // 緯度経度をデバックエリアに表示(9)
           print(tergetCoordnate)
           
-          //ピンを作成
+          //MKPointAnnotationインスタンスを取得し、ピンを作成(10)
           let pin = MKPointAnnotation()
           
-          // ピンの置く場所に緯度経度を設定
+          // ピンの置く場所に緯度経度を設定(11)
           pin.coordinate = tergetCoordnate
           
-          // ピンのタイトルを設定
+          // ピンのタイトルを設定(12)
           pin.title = searchKeyword
           
-          // ピンを地図に置く
+          // ピンを地図に置く(13)
           self.dispMap.addAnnotation(pin)
           
           // 緯度経度を中心にして半径500mの範囲を表示
-          self.dispMap.region = MKCoordinateRegionMakeWithDistance(tergetCoordnate ,500.0, 500.0 )                                      }
-  
-        
+          self.dispMap.region = MKCoordinateRegionMakeWithDistance(tergetCoordnate ,500.0, 500.0 )
+        }
       }
-  })
+    })
+    // デフォルト動作を行うのでtrueを返す(4)
     return true
     
-    }
+  }
   
   
   func mapTapped(_ sender: UITapGestureRecognizer){
-    // タッチ終了か？
-    if sender.state == .ended {
-      // 画面上のタッチした座標を取得
-      let tapPoint = sender.location(in: view)
-      // タッチした座標からマップ上の緯度経度を取得
-      let location = dispMap.convert(tapPoint, toCoordinateFrom: dispMap)
-      print(location)
-      
-      
-//      //指定した緯度経度を半径100mで円を描く
-//      let circle = MKCircle(center: location, radius: 100.0)
-//      dispMap.removeOverlays(dispMap.overlays)
-//      dispMap.add(circle)
-     
-      
-      let pin = MKPointAnnotation()
-     dispMap.removeAnnotations(dispMap.annotations)
-      pin.coordinate = location
-      dispMap.addAnnotation(pin)
     
-      self.memo = location
+    // 画面上のタッチした座標を取得
+    let tapPoint = sender.location(in: dispMap)
+    
+    // タッチした座標からマップ上の緯度経度を取得
+    let location = dispMap.convert(tapPoint, toCoordinateFrom: dispMap)
+    
+    // ToDo入力するダイヤログを生成
+    let dialog = UIAlertController(title: "memo", message: "", preferredStyle: .alert)
+    
+    // OKボタン生成
+    let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+      
+      // 入力されたテキストを保持
+      if let userInput = dialog.textFields?.first?.text {
+       
+        // MKPointAnnotationインスタンスを取得し、ピンを生成(10)
+        let pin = MKPointAnnotation()
+        
+        // ピンの置く場所に緯度経度を設定(11)
+        pin.coordinate = location
+        
+        // ピンのタイトルを設定(12)
+        pin.title = userInput
+        
+        // ピンを地図に置く(13)
+        self.dispMap.addAnnotation(pin)
+      }
       
     }
-
-
     
+    // ダイヤログをリセット
+    dialog.addTextField(configurationHandler: nil)
+    
+    // ダイヤログにボタンを付与
+    dialog.addAction(okAction)
+    
+    // ダイヤログを表示
+    present(dialog, animated: true, completion: nil)
   }
-//    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-//      //円のデザインを決める
-//      let circleRenderer = MKCircleRenderer(overlay: overlay)
-//      circleRenderer.strokeColor = UIColor.gray
-//      circleRenderer.fillColor = UIColor.gray.withAlphaComponent(0.5)
-//      circleRenderer.lineWidth = 1.0
-//      
-//      
-//      
-//      return circleRenderer
-//    }
+
   
-  @IBAction func changeMapButtonAction(_ sender: Any) {
+  func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+    mapView.selectAnnotation(self.pin, animated: true)
+  }
+  
+  
+  // 切り替えボタン
+   @IBAction func changeMapButtonAction(_ sender: Any) {
+    
+    // mapTypeプロパティ値をトグル
+    // standard(標準)
     if dispMap.mapType == .standard {
+      // satellite(航空写真)
       dispMap.mapType = .satellite
+    
     } else if dispMap.mapType == .satellite {
+     // hybrid(航空写真+標準)
       dispMap.mapType = .hybrid
+      
     } else if dispMap.mapType == .hybrid {
-    dispMap.mapType = .satelliteFlyover
+      // hybrid(3D Flyover)
+      dispMap.mapType = .satelliteFlyover
+      
     } else if dispMap.mapType == .satelliteFlyover{
+      // hybrid(3D Flyover+標準)
       dispMap.mapType = .hybridFlyover
+      
     } else {
+      // 標準に戻る
       dispMap.mapType = .standard
     }
-    }
+  }
+  
 }
 
 
